298c4f29e25ac4deb88391524018d04dda5f80a1
munozaldanasaragabriela-star <munozaldanasaragabriela@gmail.com>
2026-04-06 22:30:13 -0500

    Agregar comentario a NodoEstudiante

--- listas-estudiantes/NodoEstudiante.cs
+++ listas-estudiantes/NodoEstudiante.cs
@@ -4,6 +4,7 @@ namespace SistemaEstudiantes
     {
         public Estudiante Dato { get; set; }
         public NodoEstudiante Siguiente { get; set; }
+
         public NodoEstudiante(Estudiante dato)
         {
             Dato = dato;
